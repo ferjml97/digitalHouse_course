9f7af7245a5ba3037896fb820ea4048b4589afc0
Fernando Maldonado <47682546+ferjml97@users.noreply.github.com>
2021-03-31 20:21:27 -0600

    update_01module
    Update 01 module

---
+++ Introducción_JS/modulos/variables_tipoDatos/variablesTipoDatos.js
@@ -0,0 +1,25 @@
+
+//  DECLARACIÓN DE VARIABLES
+
+/*
+    Una variable es un espacio de memoria que podemos utilizar para almacenar valores.
+    Si queremos utilizar una variable lo primero que debemos hacer es declararla
+*/
+
+let edad;
+
+//  ALMACENANDO DATOS
+
+/*
+    Para guardar un valor en una variable utilizamos el signo IGUAL ( = )
+    y a continuación el valor que queremos almacenar. 
+    En programación, el procedimiento de almacenar un valor en una variable se denomina asignación. 
+*/
+
+let edad = 23;
+
+// TIPOS DE DATOS
+
+let edad = 23;              //NUMBER (entero y decimal)
+let nombre = 'Fernando';    //STRING (comillas simples y dobles)
+let esCierto = true;        //BOOLEAN (verdadero o falso)
