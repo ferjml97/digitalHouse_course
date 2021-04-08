e15337dcdf82330b5d3fc700f2af6508e956cdad
Fernando Maldonado <47682546+ferjml97@users.noreply.github.com>
2021-04-07 22:40:11 -0500

    Create funciones.js
    Create file funciones.js = module 2

---
+++ Introducción_JS/modulos/funciones/funciones.js
@@ -0,0 +1,16 @@
+
+//  FUNCIONES
+
+/*
+    Una función es un BLOQUE DE CÓDIGO que podemos invocar todas las veces que necesitemos
+    Puede realizar una TAREA ESPECIFICA y RETORNAR un valor.
+    Nos permite AGRUPAR el CÓDIGO que vayamos a USAR MUCHAS VECES.
+*/
+
+    function sumar (a,b){
+        return a + b;
+    }
+
+/*
+
+*/
