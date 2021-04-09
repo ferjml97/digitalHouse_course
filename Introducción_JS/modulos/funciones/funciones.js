6ecb258d9b8d20ef6433a5f1b122765eb764a5ff
Fernando Maldonado <47682546+ferjml97@users.noreply.github.com>
2021-04-08 23:38:02 -0500

    update funciones.js v3
    v3 module funciones.js

--- Introducción_JS/modulos/funciones/funciones.js
+++ Introducción_JS/modulos/funciones/funciones.js
@@ -12,5 +12,15 @@
     }
 
 /*
-
+    Ejemplo:
+    -Usa la palabra reservada (function) para indicar a JS que escribirenos una función.
+    -Definimos un nombre (sumar) para referirnos a nuestra función al momento de invocarla.
+    -Escribimos entre parentesis y entre ellos los parametros de la función ((a,b)) si lleva más de 1 
+        el siguiente lo separamos con una coma (,). Si la función no lleva parametros, escribimos
+        los parentesis sin nada adentro.
+    -Dentro de la función podemos acceder a los parametros como si fueran variables.
+    -Entre las llaves de apertura y cierre escribimos la lógica de nuestra función, es decir,
+        el código que queremos que se ejecute cada vez que la invoquemos. 
+    -Para devolver el resultado de la función utilizamos la palabra reservada (return), 
+        seguida de lo que queremos retornar.
 */
