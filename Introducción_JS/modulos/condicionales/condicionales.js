c1b4f54772a1a91345d45a7037488c9386d0d294
Fernando Maldonado <47682546+ferjml97@users.noreply.github.com>
2021-04-20 22:25:34 -0500

    mod3 condicionales.js
    Added modele 3: Condicionales.js

---
+++ Introducción_JS/modulos/condicionales/condicionales.js
@@ -0,0 +1,22 @@
+
+//  Modulo 3: CONDICIONAES
+
+/*
+ *  Las condicionales se apoyan en gran manera de los operadores lógicos.
+ */
+
+//  3.1 OPERADORES LÓGICOS
+
+/*
+ *  Nos permiten manipular el valor de las variables, realizar operaciones
+ *  y comparar sus valores.
+ *  Existen 3 operadores y (and), o (or), negación (not).
+ */
+
+//  3.1.1 AND (&&) - Todos los valores deben evaluar como true.
+
+        (10 > 15) && (10 != 20) 
+    //    false  ----   true   --->   false
+
+
+
