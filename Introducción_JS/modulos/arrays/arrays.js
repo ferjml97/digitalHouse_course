148ef35d69dcaa2a33fd7e2a814dd0c9ba9beb17
Fernando Maldonado <47682546+ferjml97@users.noreply.github.com>
2021-05-30 22:56:02 -0500

    Add arrays.js
    arrays.js v1

---
+++ Introducción_JS/modulos/arrays/arrays.js
@@ -0,0 +1,34 @@
+
+//  MODULO 4:   ARRAYS
+
+    /*
+        Los arrays nos permiten generar una COLECCIÓN de DATOS ORDENADOS
+    
+    */
+
+    //  4.2 JUEGOS DE AZAR
+
+    /*
+    Podemos representar conjuntos de números o booleanos, de igual forma: 
+    escribiéndolos entre corchetes y separados por comas. 
+    Podemos tener arrays de números, de strings, de booleanos, etc. ¡Incluso podríamos tener arrays de arrays!
+    */
+
+    /*  EJERCICIO
+        Mostrar en consola los siguientes arrays:
+    */
+
+        let numerosDeLoteria = [2, 11, 17, 32, 36, 39];
+        let tiradasDelDado = [1, 6, 6, 2, 2, 4];
+        let salioCara = [false, false, true, false];
+        let listaDeListasDeNumeros = [[1, 6], [6, 2, 2, 4]];
+    
+        console.log(numerosDeLoteria);
+        console.log(tiradasDelDado);
+        console.log(salioCara);
+        console.log(listaDeListasDeNumeros);
+
+    //4.3   ARRAY VACIO
+
+    
+    let otroArrayVacio = [];
